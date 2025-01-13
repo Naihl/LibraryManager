9cdcf916eecfb788277de9fb8d45d774ffe04199
bonnel + noah <bonnelnoah@gmail.com>
2025-01-13 10:55:31 +0100

    feat: create filter adventureBooks

--- LibraryManager.app/Program.cs
+++ LibraryManager.app/Program.cs
@@ -1,19 +1,27 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
 internal class Program
 {
     private static void Main(string[] args)
     {
         // Console.WriteLine("Hello, World!");
 
-        Book[] Library =
+        Book[] library =
         [
             new Book("Pérismer", "Fiction"),
             new Book("Le Seigneur des anneaux", "Aventure"),
             new Book("Star Wars", "SF"),
         ];
 
+        IEnumerable<Book> aventureBooks = from Book in library
+                                          where Book.Type == "Aventure"
+                                          select Book;
 
-        foreach (Book book in Library)
+
+
+        foreach (Book book in aventureBooks)
         {
             Console.WriteLine(book.Name);
             // Console.WriteLine(book.Type);
