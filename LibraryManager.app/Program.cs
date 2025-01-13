e122caff283f39a12cb43025e9ff99b7fa67e555
Colin Prokopowicz <colin.prokopowicz@c1cloud.ovh>
2025-01-13 10:40:28 +0100

    feat: using Book class in main program

--- LibraryManager.app/Program.cs
+++ LibraryManager.app/Program.cs
@@ -1,13 +1,23 @@
-﻿internal class Program
+﻿
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
-    }
-}
+        // Console.WriteLine("Hello, World!");
 
-class Book
-{
-    string name = "";
-    string type = "";
+        Book[] Library =
+        [
+            new Book("Pérismer", "Fiction"),
+            new Book("Le saigneur des angneaux", "Aventure"),
+            new Book("Star Wars", "SF"),
+        ];
+
+
+        foreach (Book book in Library)
+        {
+            Console.WriteLine(book.Name);
+            // Console.WriteLine(book.Type);
+        }
+
+    }
 }
